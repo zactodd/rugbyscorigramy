32831eec4fc2b25104ef7f3d8745860398c84395
zactodd <zactodd0@gmail.com>
2019-10-01 00:54:47 +1300

    pools updated

--- pool_outcomes.py
+++ pool_outcomes.py
@@ -13,7 +13,14 @@ SCORE_OUTCOMES = {
 }
 
 POOL_A = {"JAPAN": 9, "IRELAND": 6, "SAMOA": 5, "RUSSIA": 0, "SCOTLAND": 0}
+POOL_B = {"NEW ZEALAND": 4, "SOUTH AFRICA": 5, "ITALY": 10, "CANADA": 0, "NAMIBIA": 0}
+POOL_C = {"ENGLAND": 10, "FRANCE": 4, "ARGENTINA": 6, "TONGA": 0, "UNITED STATES": 0}
+POOL_D = {"WALES": 9, "AUSTRALIA": 6, "FIJI": 2, "URUGUAY": 4, "GEORGIA": 5}
+
 pool_a_matches = matches_teams(remaining_matches(), "A")
+pool_b_matches = matches_teams(remaining_matches(), "B")
+pool_c_matches = matches_teams(remaining_matches(), "C")
+pool_d_matches = matches_teams(remaining_matches(), "D")
 
 
 def pool_score_outcomes(current_pool, matches, match_outcomes=SCORE_OUTCOMES):
