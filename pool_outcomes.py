faa9f74cb2d208296e9478c7bf813f42b733e554
zactodd <zactodd0@gmail.com>
2019-10-01 01:46:45 +1300

    Update pool_outcomes.py

--- pool_outcomes.py
+++ pool_outcomes.py
@@ -1,4 +1,4 @@
-from itertools import product
+from itertools import product, chain
 from match_data import remaining_matches, matches_teams
 
 SCORE_OUTCOMES = {
@@ -12,7 +12,7 @@ SCORE_OUTCOMES = {
     (3, 2), (2, 3), (2, 2)
 }
 
-POOL_A = {"JAPAN": 9, "IRELAND": 6, "SAMOA": 5, "RUSSIA": 0, "SCOTLAND": 0}
+POOL_A = {"JAPAN": 9, "IRELAND": 6, "SAMOA": 5, "RUSSIA": 0, "SCOTLAND": 5}
 POOL_B = {"NEW ZEALAND": 4, "SOUTH AFRICA": 5, "ITALY": 10, "CANADA": 0, "NAMIBIA": 0}
 POOL_C = {"ENGLAND": 10, "FRANCE": 4, "ARGENTINA": 6, "TONGA": 0, "UNITED STATES": 0}
 POOL_D = {"WALES": 9, "AUSTRALIA": 6, "FIJI": 2, "URUGUAY": 4, "GEORGIA": 5}
@@ -27,14 +27,23 @@ def pool_score_outcomes(current_pool, matches, match_outcomes=SCORE_OUTCOMES):
     outcomes = [current_pool]
     for t1, t2 in matches:
         t1, t2 = t1.upper(), t2.upper()
-        new_outcomes = []
+        new_outcomes = {}
         for o, (p1, p2) in product(outcomes, match_outcomes):
             o = o.copy()
             o[t1] += p1
             o[t2] += p2
-            new_outcomes.append(o)
-        outcomes = new_outcomes
+            new_outcomes[hash(tuple(o.items()))] = o
+        outcomes = new_outcomes.values()
     return outcomes
 
 
-print(pool_score_outcomes(POOL_A, pool_a_matches))
+def pprint_pool_outcomes(outcomes):
+    row_format = " | ".join("{:12} {:3}" for _ in range(5))
+    rank_row = row_format.format(*["1st", "", "2nd", "", "3rd", "", "4th", "", "5th", ""])
+    table_format = "\n".join(["Outcome: {}", rank_row, row_format, "\n"])
+    for i, o in enumerate(outcomes):
+        results = sorted(o.items(), key=lambda x: x[1], reverse=True)
+        print(table_format.format(i, *list(chain.from_iterable(results))))
+
+
+pprint_pool_outcomes(pool_score_outcomes(POOL_D, pool_d_matches))
