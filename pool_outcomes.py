ea902b5dc8a79bb8ffef66cee84e647aac07f37b
zactodd <zactodd0@gmail.com>
2019-10-01 02:15:35 +1300

    Update pool_outcomes.py

--- pool_outcomes.py
+++ pool_outcomes.py
@@ -1,5 +1,6 @@
 from itertools import product, chain
 from match_data import remaining_matches, matches_teams
+import math
 
 SCORE_OUTCOMES = {
     # Bonus point win with bonus loss variations
@@ -33,12 +34,12 @@ def pool_score_outcomes(current_pool, matches, match_outcomes=SCORE_OUTCOMES):
             o[t1] += p1
             o[t2] += p2
             new_outcomes[hash(tuple(o.items()))] = o
-        outcomes = new_outcomes.values()
+        outcomes = list(new_outcomes.values())
     return outcomes
 
 
 def pprint_pool_outcomes(outcomes):
-    row_format = " | ".join("{:12} {:3}" for _ in range(5))
+    row_format = " | ".join("{:15} {:3}" for _ in range(5))
     rank_row = row_format.format(*["1st", "", "2nd", "", "3rd", "", "4th", "", "5th", ""])
     table_format = "\n".join(["Outcome: {}", rank_row, row_format, "\n"])
     for i, o in enumerate(outcomes):
@@ -46,4 +47,22 @@ def pprint_pool_outcomes(outcomes):
         print(table_format.format(i, *list(chain.from_iterable(results))))
 
 
-pprint_pool_outcomes(pool_score_outcomes(POOL_D, pool_d_matches))
+def agg_pool_outcomes(outcomes, transform_totals=None):
+    row_format = " | ".join("{:15} {:8}" for _ in range(5))
+    rank_row = row_format.format(*["1st", "", "2nd", "", "3rd", "", "4th", "", "5th", ""])
+    table_format = "\n".join([rank_row, row_format, "\n"])
+    ranks_dicts = {t: 0 for t in outcomes[0]}
+    for o in outcomes:
+        for t, s in sorted(o.items(), key=lambda x: x[1], reverse=True):
+            ranks_dicts[t] += s
+
+    if transform_totals is not None:
+        ranks_dicts = {t: transform_totals(s) for t, s in ranks_dicts.items()}
+    results = sorted(ranks_dicts.items(), key=lambda x: x[1], reverse=True)
+    print(table_format.format(*list(chain.from_iterable(results))))
+
+
+for i in "ABCD":
+    print("POOL: ", i)
+    trans = "lambda x: round(math.log(x), 4)"
+    exec("agg_pool_outcomes(pool_score_outcomes(POOL_{}, pool_{}_matches), {})".format(i, i.lower(), trans))
