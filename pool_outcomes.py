71df39bccc4d89a85a59e283ab14c177c7a877e7
zactodd <zactodd0@gmail.com>
2019-10-05 01:08:19 +1300

    Update pool_outcomes.py

--- pool_outcomes.py
+++ pool_outcomes.py
@@ -1,5 +1,5 @@
 from itertools import product, chain
-from match_data import remaining_matches, matches_teams
+from match_data import remaining_matches, matches_in_pool
 import math
 
 SCORE_OUTCOMES = {
@@ -18,10 +18,10 @@ POOL_B = {"NEW ZEALAND": 9, "SOUTH AFRICA": 10, "ITALY": 10, "CANADA": 0, "NAMIB
 POOL_C = {"ENGLAND": 10, "FRANCE": 9, "ARGENTINA": 6, "TONGA": 0, "UNITED STATES": 0}
 POOL_D = {"WALES": 9, "AUSTRALIA": 6, "FIJI": 7, "URUGUAY": 4, "GEORGIA": 5}
 
-pool_a_matches = matches_teams(remaining_matches(), "A")
-pool_b_matches = matches_teams(remaining_matches(), "B")
-pool_c_matches = matches_teams(remaining_matches(), "C")
-pool_d_matches = matches_teams(remaining_matches(), "D")
+pool_a_matches = matches_in_pool(remaining_matches(), "A")
+pool_b_matches = matches_in_pool(remaining_matches(), "B")
+pool_c_matches = matches_in_pool(remaining_matches(), "C")
+pool_d_matches = matches_in_pool(remaining_matches(), "D")
 
 
 def pool_score_outcomes(current_pool, matches, match_outcomes=SCORE_OUTCOMES):
@@ -47,13 +47,13 @@ def pprint_pool_outcomes(outcomes):
         print(table_format.format(i, *list(chain.from_iterable(results))))
 
 
-def agg_pool_outcomes(outcomes, transform_totals=None):
+def agg_pool_outcomes(outcomes, transform_totals=None, position_till=-1):
     row_format = " | ".join("{:15} {:8}" for _ in range(5))
     rank_row = row_format.format(*["1st", "", "2nd", "", "3rd", "", "4th", "", "5th", ""])
     table_format = "\n".join([rank_row, row_format, "\n"])
     ranks_dicts = {t: 0 for t in outcomes[0]}
     for o in outcomes:
-        for t, s in sorted(o.items(), key=lambda x: x[1], reverse=True):
+        for t, s in sorted(o.items(), key=lambda x: x[1], reverse=True)[0:position_till]:
             ranks_dicts[t] += s
 
     if transform_totals is not None:
@@ -64,5 +64,5 @@ def agg_pool_outcomes(outcomes, transform_totals=None):
 
 for i in "ABCD":
     print("POOL: ", i)
-    trans = None  # "lambda x: round(math.log(x), 4)"
+    trans = None  # "lambda x: round(math.log(x), 4) if x > 0 else 0"
     exec("agg_pool_outcomes(pool_score_outcomes(POOL_{}, pool_{}_matches), {})".format(i, i.lower(), trans))
