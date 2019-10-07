a0fa156560f7450dc923d657d1437a0ce3671f43
zactodd <zactodd0@gmail.com>
2019-10-07 17:14:13 +1300

    Update pool_outcomes.py

--- pool_outcomes.py
+++ pool_outcomes.py
@@ -14,7 +14,7 @@ SCORE_OUTCOMES = {
 }
 
 POOL_A = {"JAPAN": 14, "IRELAND": 11, "SAMOA": 5, "RUSSIA": 0, "SCOTLAND": 5}
-POOL_B = {"NEW ZEALAND": 14, "SOUTH AFRICA": 10, "ITALY": 10, "CANADA": 0, "NAMIBIA": 0}
+POOL_B = {"NEW ZEALAND": 14, "SOUTH AFRICA": 15, "ITALY": 10, "CANADA": 0, "NAMIBIA": 0}
 POOL_C = {"ENGLAND": 15, "FRANCE": 13, "ARGENTINA": 6, "TONGA": 1, "UNITED STATES": 0}
 POOL_D = {"WALES": 9, "AUSTRALIA": 11, "FIJI": 7, "URUGUAY": 4, "GEORGIA": 5}
 
@@ -64,5 +64,5 @@ def agg_pool_outcomes(outcomes, transform_totals=None, position_till=-1):
 
 for i in "ABCD":
     print("POOL: ", i)
-    trans = None  # "lambda x: round(math.log(x), 4) if x > 0 else 0"
+    trans = "lambda x: round(math.log(x), 4) if x > 0 else 0"
     exec("agg_pool_outcomes(pool_score_outcomes(POOL_{}, pool_{}_matches), {}, {})".format(i, i.lower(), trans, 2))
